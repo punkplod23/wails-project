4c29f8ca5af62e3319ece5cc5e32076f6ee365cd
gareth gwyther <gareth.gwyther@gmail.com>
2024-12-01 03:58:32 +0000

    inverted-index

--- internal/parsecsv/parsecsv.go
+++ internal/parsecsv/parsecsv.go
@@ -1,24 +1,39 @@
 package parsecsv
 
 import (
+	"bufio"
 	"context"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/labstack/gommon/log"
 )
 
+type FilePosition struct {
+	bytePosition int64
+	offset       int
+}
+
+type InvertedIndex struct {
+	Term          string
+	FilePositions []FilePosition
+}
+
 type CsvParser struct {
-	ctx         context.Context
-	FilePath    string
-	FileOutName string
-	FileWriter  *os.File
-	header      []string
-	row         [][]string
-	record      []string
-	CSVReader   *csv.Reader
+	ctx                context.Context
+	FilePath           string
+	FileOutName        string
+	FileWriter         *os.File
+	header             []string
+	row                [][]string
+	record             []string
+	CSVReader          *csv.Reader
+	WordListAjdectives map[string]struct{}
+	Tokens             map[string]InvertedIndex
 }
 
 type csvRow struct {
@@ -32,11 +47,40 @@ func NewCSVParser(ctx context.Context) *CsvParser {
 	return CsvParser
 }
 
+func (parser *CsvParser) createWordListAjdectives() map[string]struct{} {
+	file, err := os.Open("assets/word-list.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+	wordListAdjectives := make(map[string]struct{})
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Process the line here
+		wordListAdjectives[strings.ToLower(line)] = struct{}{}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return wordListAdjectives
+}
+
 func (parser *CsvParser) RunFile(filePath string) string {
 	fmt.Println(filePath)
 	parser.FilePath = filePath
+	parser.WordListAjdectives = parser.createWordListAjdectives()
+	//fmt.Println(parser.WordListAjdectives)
 	parser.reader()
 	parser.processCSV()
+	//fmt.Println(readStringFromPositionWithOffset("C:\\github\\wails-project\\test.json", 2609164, 362))
+	parser.searchForResults("BDCQ.SF8RS1CA 2024.03")
 	return parser.complete()
 }
 
@@ -65,6 +109,7 @@ func (parser *CsvParser) processCSV() {
 	i := 0
 	delimeter := ""
 	json := ""
+	parser.Tokens = make(map[string]InvertedIndex)
 	for {
 		record, err := parser.CSVReader.Read()
 		if err != nil {
@@ -85,22 +130,136 @@ func (parser *CsvParser) processCSV() {
 				delimeter = ","
 			}
 			json = delimeter + "{"
-
+			var tokens = make(map[string]struct{})
 			for itter, header := range parser.header {
 				if itter > 0 {
 					json += ","
 				}
+
+				for key, token := range parser.createTokens(string(parser.record[itter])) {
+					tokens[key] = token
+				}
+
 				json += string(header) + ":" + string(parser.record[itter])
 
 			}
 			json += "}"
+
 			if _, err := parser.FileWriter.Write([]byte(json)); err != nil {
 				panic(err)
 			}
+
+			byteSize, err := parser.FileWriter.Seek(0, io.SeekCurrent)
+			if err != nil {
+				fmt.Println("Error getting file position:", err)
+				return
+			}
+
+			for key, _ := range tokens {
+				if _, ok := parser.Tokens[key]; ok {
+					indexCopy := parser.Tokens[key]
+					indexCopy.FilePositions = append(indexCopy.FilePositions, FilePosition{bytePosition: byteSize, offset: len(json)})
+					parser.Tokens[key] = indexCopy
+				} else {
+					newIndex := InvertedIndex{
+						Term: key,
+						FilePositions: []FilePosition{
+							{bytePosition: byteSize, offset: len(json)},
+						},
+					}
+					parser.Tokens[key] = newIndex
+				}
+
+			}
+
 		}
 
 		i++
 	}
+	//fmt.Println(parser.Tokens)
+}
+
+func (parser *CsvParser) searchForResults(search string) {
+
+	result := parser.createTokens(search)
+	termResults := make(map[FilePosition]int)
+
+	var kvPairs []struct {
+		key   FilePosition
+		value int
+	}
+
+	for term, _ := range result {
+		if _, ok := parser.Tokens[term]; ok {
+			for _, FP := range parser.Tokens[term].FilePositions {
+				if _, ok := termResults[FP]; ok {
+					termResults[FP] += 1
+				} else {
+					termResults[FP] = 1
+				}
+			}
+		}
+	}
+
+	for k, v := range termResults {
+		kvPairs = append(kvPairs, struct {
+			key   FilePosition
+			value int
+		}{k, v})
+	}
+
+	sort.Slice(kvPairs, func(i, j int) bool {
+		return kvPairs[i].value > kvPairs[j].value
+	})
+
+	limitedPairs := kvPairs[:10]
+	for _, pair := range limitedPairs {
+		go fmt.Println(readStringFromPositionWithOffset("C:\\github\\wails-project\\test.json", pair.key.bytePosition, int64(pair.key.offset)))
+	}
+}
+
+func (parser *CsvParser) createTokens(record string) map[string]struct{} {
+
+	replaceStr := strings.Replace(string(record), ",", " ", -1)
+	replaceStr = strings.Replace(string(record), ".", " ", -1)
+	replaceStr = strings.Replace(string(record), ":", " ", -1)
+	result := strings.Split(string(replaceStr), " ")
+	tokens := make(map[string]struct{})
+	for _, line := range result {
+		if line == "" {
+			continue
+		}
+		if _, ok := parser.WordListAjdectives[line]; ok {
+			continue
+		}
+
+		tokens[strings.ToLower(line)] = struct{}{}
+
+	}
+
+	return tokens
+
+}
+
+func readStringFromPosition(filename string, bytePosition int64) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	_, err = file.Seek(bytePosition, io.SeekStart)
+	if err != nil {
+		return "", err
+	}
+
+	buf := make([]byte, 100) // Adjust buffer size as needed
+	n, err := file.Read(buf)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return string(buf[:n]), nil
 }
 
 func (parser *CsvParser) complete() string {
@@ -113,4 +272,25 @@ func (parser *CsvParser) complete() string {
 	fmt.Println(parser.FileWriter)
 	return "Parsing complete"
 
+}
+
+func readStringFromPositionWithOffset(filename string, bytePosition int64, offset int64) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.Seek(bytePosition-offset, io.SeekStart)
+	if err != nil {
+		return "", fmt.Errorf("failed to seek to byte position: %w", err)
+	}
+
+	buf := make([]byte, offset) // Adjust buffer size as needed
+	n, err := file.Read(buf)
+	if err != nil && err != io.EOF {
+		return "", fmt.Errorf("failed to read from file: %w", err)
+	}
+
+	return string(buf[:n]), nil
 }
